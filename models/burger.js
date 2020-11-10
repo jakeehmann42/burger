3695a291ae92ddf063d65453e8b41da8f3407731
Jakeehmann42 <Jakeehmann42@gmail.com>
2020-11-10 15:06:34 -0700

    orm.js, and burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,22 @@
 const orm = require("../config/orm.js");
+const burger = {
+    create: function(name,cb) {
+        orm.create("burgers", [
+            "burger_name",
+            "devoured"
+        ],
+        [
+            name, false
+        ],cb)
+    },
+
+    all: function(cb) {
+        orm.all ("burgers", function(res) {
+            cb(res)
+        })
+    } 
+    //update:
+}
 
 
 
